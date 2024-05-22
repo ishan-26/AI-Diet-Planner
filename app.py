56c18bb89c556b56e1c29a53d3210f23e3ce7a5f
ishan-26 <125141868+ishan-26@users.noreply.github.com>
2024-05-22 19:03:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ input_prompt_diet = """
     breakfast, lunch, dinner with respect to the given items provided by the user. Do not include any items apart from input provided.
     Also if possible provide dish along with the receipes.
 
-    If the input contains number of calories, then create a Indian meal plan within a total calorie limit for breakfast, lunch and dinner provided by the user.
+    If the input contains total number of calories, then create a Indian meal plan within a total calorie limit provided by the user for breakfast, lunch and dinner.
  
     Only respond if the image pertains to food items else respond with not appropriate items mentioned.
     """
